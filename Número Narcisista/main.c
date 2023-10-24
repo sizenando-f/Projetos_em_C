4696c135d16b7a5ec71aeb3aeb10e6e260ffdc57
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2023-10-24 16:50:19 -0400

    Menu principal implementado

--- Número Narcisista/main.c
+++ Número Narcisista/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <windows.h> // unistd.h - sleep() ou a usleep()
 
 int numero_narcisista(int num, int algarismos){
   int soma = 0;
-  
+
   for(int i = 0; i < algarismos; i++){
     soma += pow((num%10), algarismos);
     num /= 10;
@@ -26,9 +27,26 @@ int descobre_algarismos(int num){
 }
 
 int main(){
-  int fim, indice = 0;
-  printf("Insira um valor: ");
-  scanf("%d", &fim);
+  int fim, indice = 0, esc;
+  do{
+    printf("-----# NUM3R0 N4RC1S1ST4 #-----\n");
+    printf(" 1. ARMAZENAR NUMEROS\n");
+    printf(" 2. EXCLUIR NUMERO\n");
+    printf(" 3. MOSTRAR NUMEROS NARCISISTAS\n");
+    printf(" 4. SAIR\n");
+    printf("-------------------------------\n");
+    printf("ESCOLHA ~# ");
+    scanf("%d", &esc);
+    switch (esc){
+      case 1:
+        printf("INSIRA UM VALOR LIMITE: ");
+        scanf("%d", &fim);
+      default:
+        break;
+    }
+  }while(esc != 4);
+
+  
   int * numeros = malloc(fim * sizeof(int));
 
   for(int i = 10; i <= fim; i++){
