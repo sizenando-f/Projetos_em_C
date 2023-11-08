3d4c6cb25916f25489b998b34abbc47325358801
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2023-11-08 09:43:22 -0400

    Implementação de linhas numeradas

--- Decifra Arquivo/main.c
+++ Decifra Arquivo/main.c
@@ -19,7 +19,6 @@ void converte_texto(char texto_convertido[99000]){
         // Usa strtok para separar os números entre colchetes
         char *numero_char = strtok(texto, "[]");
         while (numero_char != NULL) {
-            
             int numero_int = atoi(numero_char);
             char letra = (char)numero_int;
             // Concatena o caractere à string texto_convertido
@@ -30,47 +29,37 @@ void converte_texto(char texto_convertido[99000]){
     fclose(fp);
 }
 
-void formata_texto(char texto[99000], char texto_formatado[712][56]) {
+void formata_texto(char texto[99000], char texto_formatado[726][56]) {
     int linha = 0;
     int coluna = 0;
-    int i = 0;
+    int i = 0, cont = 0;
     
     while (texto[i] != '\0') {
-        if (texto[i] == '\n') {
-            // Se encontrarmos um '\n', incluímos na linha atual
-            texto_formatado[linha][coluna] = texto[i];
-            linha++;  // Avançamos para a próxima linha
-            coluna = 0;  // Reiniciamos a coluna
+        if (texto[i] == '\n' || cont == 56) {
+            texto_formatado[linha][coluna] = '\0';
+            linha++;  
+            coluna = 0; 
+            cont = 0;
         } else {
             texto_formatado[linha][coluna] = texto[i];
+            coluna++;
         }
-
-        coluna++;
         i++;
+        cont++;
     }
 }
 
-void exibe_linhas_com_numero(char texto_formatado[712][56]) {
-    for (int i = 0; i < 712; i++) {
-        // Verifica se a linha contém apenas espaços em branco
-        int j;
-        for (j = 0; j < 56; j++) {
-            if (texto_formatado[i][j] != ' ' && texto_formatado[i][j] != '\n' && texto_formatado[i][j] != '\0') {
-                break;
-            }
-        }
-        if (j < 56) {
-            printf("%d: %s", i, texto_formatado[i]);
-        }
+void exibe_linhas_com_numero(char texto[726][56]){
+    for(int i = 0; i < 726; i++){
+        printf("[linha: %d] %s\n", i+1, texto[i]);
     }
 }
-
 
 int main() {
     char texto_convertido[99000];
     converte_texto(texto_convertido);
 
-    char texto_formatado[710][56];
+    char texto_formatado[726][56];
     formata_texto(texto_convertido, texto_formatado);
 
     exibe_linhas_com_numero(texto_formatado);
