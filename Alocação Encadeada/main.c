9a3ecf0f5e1469d824a03c92849ca4461021bdf2
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-05-28 11:19:25 -0400

    Work finalization

--- Alocação Encadeada/main.c
+++ Alocação Encadeada/main.c
@@ -25,7 +25,7 @@ struct ListaDup{
 void buscaSEO(int x, struct Lista **L, struct Lista **ant, struct Lista **pont){
   *ant = NULL;
   *pont = NULL;
-  struct Lista *ptr = *L;
+  struct Lista *ptr = (*L);
   while(ptr != NULL){
     if(ptr->num < x){
       *ant = ptr;
@@ -34,7 +34,8 @@ void buscaSEO(int x, struct Lista **L, struct Lista **ant, struct Lista **pont){
       *pont = ptr;
       return;
     } else {
-      ptr = NULL;
+		*pont = ptr;
+    	ptr = NULL;
     }
   }
 }
@@ -51,6 +52,12 @@ void insereSEO(int x, struct Lista **L){
 
   struct Lista *ant, *pont;
   buscaSEO(x, L, &ant, &pont);
+  if(ant == NULL){
+  	ptr->num = x;
+  	ptr->prox = pont;
+	(*L) = ptr;
+	return;
+  }
   ptr->num = x;
   ptr->prox = ant->prox;
   ant->prox = ptr;
@@ -76,44 +83,50 @@ void removeSEO(struct Lista **inicio, int num){
   }
 }
 
-void buscaCSEO(int x, struct Lista **L, struct Lista **ant, struct Lista **pont){
-  *ant = NULL;
-  *pont = NULL;
-  struct Lista *ptr = *L;
+void buscaCSEO(int x, struct Lista **L, struct Lista **ant, struct Lista **pont) {
+    (*ant) = NULL;
+    (*pont) = NULL;
+    struct Lista *ptr = (*L);
 
-  do{
-    if(ptr->num < x){
-      *ant = ptr;
-      ptr = ptr->prox;
-    } else if(ptr->num == x){
-      *pont = ptr;
-      return;
-    } else {
-      ptr = (*L);
-    }
-  }while(ptr != (*L));
+    do {
+        if (ptr->num < x) {
+            (*ant) = ptr;
+            ptr = ptr->prox;
+        } else if (ptr->num == x) {
+            (*pont) = ptr;
+            return;
+        } else {
+            (*pont) = ptr;
+            ptr = (*L);
+        }
+    } while (ptr != (*L));
 }
 
-void insereCSEO(struct Lista **inicio, int num){
-  struct Lista *no = (struct Lista *) malloc(sizeof(struct Lista));
-  no->num = num;
-  if((*inicio) == NULL){
-    (*inicio) = no;
-    no->prox = (*inicio);
-    return;
-  }
+void insereCSEO(struct Lista **inicio, int num) {
+    struct Lista *no = (struct Lista *) malloc(sizeof(struct Lista));
+    no->num = num;
 
-  struct Lista *ant, *pont;
-  buscaCSEO(num, inicio, &ant, &pont);
+    if ((*inicio) == NULL) {
+        (*inicio) = no;
+        no->prox = (*inicio);
+        return;
+    }
 
-  if(ant == NULL){
-    no->prox = (*inicio);
-    (*inicio)->prox = no;
-    return;
-  }
+    struct Lista *ant, *pont;
+    buscaCSEO(num, inicio, &ant, &pont);
 
-  no->prox = ant->prox;
-  ant->prox = no;
+    if (ant == NULL) {
+        no->prox = (*inicio);
+        struct Lista *temp = (*inicio);
+        while (temp->prox != (*inicio)) {
+            temp = temp->prox;
+        }
+        temp->prox = no;
+        (*inicio) = no;
+    } else {
+		no->prox = ant->prox;
+        ant->prox = no;
+    }
 }
 
 void removeCSEO(struct Lista **inicio, int num){
@@ -188,7 +201,6 @@ void removeDENC(struct ListaDup **L, int num){
       temp = ptr;
       ptr->ant->prox  = ptr->prox;
       free(temp);
-      return;
     }
     ptr = ptr->prox;
   }
@@ -204,11 +216,11 @@ void insereLIFO(int pilha[], int n, int *topo, int valor){
 }
 
 void removeLIFO(int *topo){
-  if((*topo) != 0){
-    (*topo) = (*topo) - 1;
-    return;
-  }
-  printf("A pilha esta vazia!\n");
+	if((*topo) != -1){
+		(*topo) = (*topo) - 1;
+		return;
+	}
+	printf("A pilha esta vazia!\n");
 }
 
 void insereFIFO(int fila[], int n, int *fim, int valor){
@@ -222,16 +234,330 @@ void insereFIFO(int fila[], int n, int *fim, int valor){
 }
 
 void removeFIFO(int fila[], int *fim){
-  for(int i = 0; i < (*fim); i++){
-    fila[i] = fila[i+1];
-  }
-  (*fim) = (*fim) - 1;
+	if((*fim) != -1){
+		for(int i = 0; i < (*fim); i++){
+		fila[i] = fila[i+1];
+		}
+		(*fim) = (*fim) - 1;
+	}
+	printf("A fila esta vazia!\n");
+}
+
+int retornaIndiceDENC(struct ListaDup **L, int valor){
+	if((*L)->prox == NULL) return 0;
+	int indice = 0;
+	struct ListaDup *p = (*L)->prox;
+	while(p != NULL){
+		if(p->num == valor){
+			return indice + 2;
+		}
+		indice++;
+		p = p->prox;
+	}
+	return 0;
+}
+
+int retornaIndice(struct Lista **L, int valor){
+	if((*L) == NULL) return 0;
+	int indice = 0;
+	struct Lista *p = (*L);
+	while(p != NULL){
+		if(p->num == valor){
+			return indice + 1;
+		}
+		indice++;
+		p = p->prox;
+	}
+	return 0;
+}
+
+int retornaIndiceCSEO(struct Lista **L, int valor){
+	if((*L) == NULL) return 0;
+	int indice = 0;
+	struct Lista *p = (*L);
+	do{
+		if(p->num == valor){
+			return indice + 1;
+		}
+		indice++;
+		p = p->prox;
+	}while(p != (*L));
+	return 0;
+}
+
+void desalocaDENC(struct ListaDup **L){
+	if((*L) == NULL) return;
+	struct ListaDup *p = (*L), *temp;
+	while(p != NULL){
+		temp = p->prox;
+		free(p);
+		p = temp;
+	}
+}
+
+void desaloca(struct Lista **L){
+	if((*L) == NULL) return;
+	struct Lista *p = (*L), *temp;
+	while(p != NULL){
+		temp = p->prox;
+		free(p);
+		p = temp;
+	}
+}
+
+void desalocaCSEO(struct Lista **L){
+	if((*L) == NULL) return;
+	struct Lista *p = (*L), *temp;
+	do{
+		temp = p;
+		p = p->prox;
+		free(temp);
+	}while(p != (*L));
+}
+
+void menuFIFO(){
+	int n = 10, fila[n], fim = -1;
+	int esc, valor;
+	char num[100] = "";
+	do{
+		int x1 = 10, x2 = 60, y = 18;
+		printf(" - Fila -\n");
+		printf("[ 1 ] Insercao\n");
+		printf("[ 2 ] Remocao\n");
+		printf("[ 3 ] Sair\n");
+		printf("Selecione a operacao > ");
+		scanf("%d", &esc);
+		switch(esc){
+			case 1:
+				printf("Insira o valor do no > ");
+				scanf("%d", &valor);
+				getchar();
+				insereFIFO(fila, n, &fim, valor);
+				break;
+			case 2:
+				removeFIFO(fila, &fim);
+				break;
+			case 3:
+				printf("Programa encerrado\n");
+				gfx_quit();
+				return;
+			default:
+				printf("Opcao invalida, tente novamente!\n");
+				break;
+		}
+		gfx_quit();
+		gfx_init(800, 300, "Fila");
+		for(int i = 0; i <= fim; i++){
+			sprintf(num, "%d", fila[i]);
+			gfx_rectangle(x1, 10, x2, 40);
+			gfx_text(x1+((x2-x1)/2)-2, y, num);
+			gfx_paint();
+			x1 = x2;
+			x2 = x1 + 50;
+		}
+	}while(esc != 4);
+}
+
+void menuLIFO(){
+	int n = 10, pilha[n], topo = -1;
+	int esc, valor;
+	char num[100] = "";
+	do{
+		int x1 = 10, x2 = 60, y = 18;
+		printf(" - Pilha -\n");
+		printf("[ 1 ] Insercao\n");
+		printf("[ 2 ] Remocao\n");
+		printf("[ 3 ] Sair\n");
+		printf("Selecione a operacao > ");
+		scanf("%d", &esc);
+		switch(esc){
+			case 1:
+				printf("Insira o valor do no > ");
+				scanf("%d", &valor);
+				getchar();
+				insereLIFO(pilha, n, &topo, valor);
+				break;
+			case 2:
+				removeLIFO(&topo);
+				break;
+			case 3:
+				printf("Programa encerrado\n");
+				gfx_quit();
+				return;
+			default:
+				printf("Opcao invalida, tente novamente!\n");
+				break;
+		}
+		gfx_quit();
+		gfx_init(800, 300, "Pilha");
+		for(int i = 0; i <= topo; i++){
+			sprintf(num, "%d", pilha[i]);
+			gfx_rectangle(x1, 10, x2, 40);
+			gfx_text(x1+((x2-x1)/2)-2, y, num);
+			gfx_paint();
+			x1 = x2;
+			x2 = x1 + 50;
+		}
+	}while(esc != 4);
+}
+
+void menuDENC(){
+	struct ListaDup *L = NULL;
+  	inicializaDENC(&L);
+  	struct ListaDup *p;
+	int esc, valor, cont, temp;
+	char num[100] = "";
+	do{
+		int x1 = 10, x2 = 60, y = 18;
+		cont = 0;
+		printf(" - Lista Duplamente Encadeada Com No Cabeca -\n");
+		printf("[ 1 ] Insercao\n");
+		printf("[ 2 ] Remocao\n");
+		printf("[ 3 ] Busca\n");
+		printf("[ 4 ] Sair\n");
+		printf("Selecione a operacao > ");
+		scanf("%d", &esc);
+		switch(esc){
+			case 1:
+				printf("Insira o valor do no > ");
+				scanf("%d", &valor);
+				getchar();
+				insereDENC(&L, valor);
+				break;
+			case 2:
+				printf("Insira o valor que deseja remover > ");
+				scanf("%d", &valor);
+				getchar();
+				removeDENC(&L, valor);
+				break;
+			case 3:
+				printf("Qual valor deseja buscar? > ");
+				scanf("%d", &valor);
+				getchar();
+				temp = retornaIndiceDENC(&L, valor);
+				if(temp){
+					printf("Valor encontrado em no [%d]\n", temp);
+				} else {
+					printf("Valor nao encontrado na lista!\n");
+				}
+				break;
+			case 4:
+				printf("Programa encerrado, a lista foi desalocada!\n");
+				desalocaDENC(&L);
+				gfx_quit();
+				return;
+			default:
+				printf("Opcao invalida, tente novamente!\n");
+				break;
+		}
+		gfx_quit();
+		gfx_init(800, 300, "Lista Duplamente Encadeada Com No Cabeca");
+		p = L;
+		while(p != NULL){
+			sprintf(num, "%d", p->num);
+			gfx_rectangle(x1, 10, x2, 40);
+			gfx_text(x1+((x2-x1)/2)-2, y, num);
+			if(cont){
+				gfx_line(x1, 25, x1 - 20, 25);
+				gfx_line(x1-7, 20, x1, 25);
+				gfx_line(x1-7, 30, x1, 25);
+				gfx_line(x1-13, 20, x1-20, 25);
+				gfx_line(x1-13, 30, x1-20, 25);
+
+			}
+			gfx_paint();
+			x1 = x2 + 20;
+			x2 = x1 + 50;
+			p = p->prox;
+			cont = 1;
+		}
+	}while(esc != 4);
+}
+
+void menuCSEO(){
+	struct Lista *L = NULL;
+  	struct Lista *p;
+	int esc, valor, cont, temp;
+	int iniX = 0, fimX = 0;
+	char num[100] = "";
+	do{
+		int x1 = 10, x2 = 60, y = 18;
+		cont = 0;
+		printf(" - Lista Circular Simplesmente Encadeada Ordenada Sem No Cabeca -\n");
+		printf("[ 1 ] Insercao\n");
+		printf("[ 2 ] Remocao\n");
+		printf("[ 3 ] Busca\n");
+		printf("[ 4 ] Sair\n");
+		printf("Selecione a operacao > ");
+		scanf("%d", &esc);
+		switch(esc){
+			case 1:
+				printf("Insira o valor do no > ");
+				scanf("%d", &valor);
+				getchar();
+				insereCSEO(&L, valor);
+				break;
+			case 2:
+				printf("Insira o valor que deseja remover > ");
+				scanf("%d", &valor);
+				getchar();
+				removeCSEO(&L, valor);
+				break;
+			case 3:
+				printf("Qual valor deseja buscar? > ");
+				scanf("%d", &valor);
+				getchar();
+				temp = retornaIndiceCSEO(&L, valor);
+				if(temp){
+					printf("Valor encontrado em no [%d]\n", temp);
+				} else {
+					printf("Valor nao encontrado na lista!\n");
+				}
+				break;
+			case 4:
+				printf("Programa encerrado, a lista foi desalocada!\n");
+				desalocaCSEO(&L);
+				gfx_quit();
+				return;
+			default:
+				printf("Opcao invalida, tente novamente!\n");
+				break;
+		}
+		gfx_quit();
+		gfx_init(800, 300, "Lista Circular Simplesmente Encadeada Ordenada Sem No Cabeca");
+		p = L;
+
+		iniX = x1+((x2-x1)/2)-2;
+		gfx_line(iniX, 40, iniX, 60);
+		gfx_line(iniX, 40, iniX+7, 47);
+		gfx_line(iniX, 40, iniX-7, 47);
+		do{
+			sprintf(num, "%d", p->num);
+			gfx_rectangle(x1, 10, x2, 40);
+			gfx_text(x1+((x2-x1)/2)-2, y, num);
+			if(p->prox == L){
+				fimX = x1+((x2-x1)/2)-2;
+				gfx_line(fimX, 40, fimX, 60);
+				gfx_line(iniX, 60, fimX, 60);
+			}
+			if(cont){
+				gfx_line(x1, 25, x1 - 20, 25);
+				gfx_line(x1-7, 20, x1, 25);
+				gfx_line(x1-7, 30, x1, 25);
+			}
+			gfx_paint();
+			x1 = x2 + 20;
+			x2 = x1 + 50;
+			p = p->prox;
+			cont = 1;
+		}while(p != L);
+	}while(esc != 4);
 }
 
 void menuSEO(){
 	struct Lista *L = NULL;
   	struct Lista *p;
-	int esc, valor, cont;
+	int esc, valor, cont, temp;
 	char num[100] = "";
 	do{
 		int x1 = 10, x2 = 60, y = 18;
@@ -245,12 +571,35 @@ void menuSEO(){
 		scanf("%d", &esc);
 		switch(esc){
 			case 1:
-				printf("Insira o valor do no >");
+				printf("Insira o valor do no > ");
 				scanf("%d", &valor);
 				getchar();
 				insereSEO(valor, &L);
 				break;
+			case 2:
+				printf("Insira o valor que deseja remover > ");
+				scanf("%d", &valor);
+				getchar();
+				removeSEO(&L, valor);
+				break;
+			case 3:
+				printf("Qual valor deseja buscar? > ");
+				scanf("%d", &valor);
+				getchar();
+				temp = retornaIndice(&L, valor);
+				if(temp){
+					printf("Valor encontrado em no [%d]\n", temp);
+				} else {
+					printf("Valor nao encontrado na lista!\n");
+				}
+				break;
+			case 4:
+				printf("Programa encerrado, a lista foi desalocada!\n");
+				desaloca(&L);
+				gfx_quit();
+				return;
 			default:
+				printf("Opcao invalida, tente novamente!\n");
 				break;
 		}
 		gfx_quit();
@@ -262,6 +611,8 @@ void menuSEO(){
 			gfx_text(x1+((x2-x1)/2)-2, y, num);
 			if(cont){
 				gfx_line(x1, 25, x1 - 20, 25);
+				gfx_line(x1-7, 20, x1, 25);
+				gfx_line(x1-7, 30, x1, 25);
 			}
 			gfx_paint();
 			x1 = x2 + 20;
@@ -270,67 +621,39 @@ void menuSEO(){
 			cont = 1;
 		}
 	}while(esc != 4);
-	gfx_quit();
-
 }
 
 int main(){
-  // Uso da SEO
-  // struct Lista *L = NULL;
-  // struct Lista *p;
-  // p = L;
-  // while(p != NULL){
-  //   printf("%d ", p->num);
-  //   p = p->prox;
-  // }
-
-  // Uso do CSEO
-  // struct Lista *L = NULL;
-  // struct Lista *p;
-  // p = L;
-  // do{
-  //   printf("%d ", p->num);
-  //   p = p->prox;
-  // }while(p != L);
-
-  // Uso do DENC
-  // struct ListaDup *L = NULL;
-  // inicializaDENC(&L);
-  // struct ListaDup *p;
-  // p = L->prox;
-  // while(p != NULL){
-  //   printf("%d ", p->num);
-  //   p = p->prox;
-  // }
-
-  // Uso do LIFO
-  // int n = 10, pilha[n], topo = -1;
-  // for(int i = 0; i <= topo; i++){
-  //   printf("%d ", pilha[i]);
-  // }
-  
-  // Uso do FIFO
-  // int n = 10, fila[n], fim = -1;
-  // for(int i = 0; i <= fim; i++){
-  //   printf("%d ", fila[i]);
-  // }
-
 	int esc;
 	printf("		-- MENU --\n");
 	printf("[ 1 ] Lista Simplesmente Encadeada Ordenada Sem No Cabeca\n");
 	printf("[ 2 ] Lista Circular Simplesmente Encadeada Ordenada Sem No Cabeca\n");
 	printf("[ 3 ] Duplamente Encadeada Com No Cabeca\n");
-	printf("[ 4 ] Fila\n");
-	printf("[ 5 ] Pilha\n");
-	printf("Qual estrutura deseja usar? > ");
-	scanf("%d", &esc);
-	switch(esc){
-		case 1:
-			menuSEO();
-			break;
-		default:
-			break;
-	}
-
+	printf("[ 4 ] Pilha\n");
+	printf("[ 5 ] Fila\n");
+	do{
+		printf("Qual estrutura deseja usar? > ");
+		scanf("%d", &esc);
+		switch(esc){
+			case 1:
+				menuSEO();
+				break;
+			case 2:
+				menuCSEO();
+				break;
+			case 3:
+				menuDENC();
+				break;
+			case 4:
+				menuLIFO();
+				break;
+			case 5:
+				menuFIFO();
+				break;
+			default:
+				printf("Opcao invalida, tente novamente!\n");
+				break;
+		}
+	}while(esc < 1 || esc > 5);
   return 0;
 }
