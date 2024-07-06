914118895dbdbad2505a800d1bc035bc3da7df65
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-07-06 12:34:06 -0400

    Inserção e exibição da matriz

---
+++ 2024/Cálculo de Determinante/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Exibe a matriz.
+ * Será exibido a matriz no console
+ * @param matriz Matriz de no máximo 10x10
+ * @param ordem Dimensão da matriz
+ */
+void exibeMatriz(int matriz[10][10], unsigned ordem){
+  for(unsigned i = 0; i < ordem; i++){
+    printf("| ");
+    for(unsigned j = 0; j < ordem; j++){
+    printf("%d ", matriz[i][j]);
+    }
+    printf("|\n");
+  }
+}
+/**
+ * @brief Cria a matriz.
+ * Usuário inicializa os valores da matriz seguido de uma confirmação onde será repetido indefinidas vezes até que o usuário aceite a matriz.
+ * @param matriz Matriz de no máximo 10x10
+ * @param ordem Ponteiro para armazenar a dimensão da matriz
+ */
+void defineMatriz(int matriz[10][10], unsigned *ordem){
+  char esc;
+  printf("[ -> ] Inicialzando matriz...\n");
+  do{
+    printf("[ <- ] Insira a ordem da matriz > ");
+    scanf("%d", &(*ordem));
+    for(unsigned i = 0; i < *ordem; i++){
+      printf("------- Linha %d --------\n", i+1);
+      for(unsigned j = 0; j < *ordem; j++){
+        printf("Coluna %d >", j+1);
+        scanf("%d", &matriz[i][j]);
+      }
+    }
+    system("cls");
+    printf("[ -> ] A matriz criada foi: \n");
+    exibeMatriz(matriz, *ordem);
+    printf("[ <- ] Deseja continuar? (S/n) > ");
+    scanf(" %c", &esc);
+  } while(esc == 'N' || esc == 'n');
+}
+
+int main(){
+  int matriz[10][10];
+  char esc;
+  unsigned ordem;
+  do{
+    defineMatriz(matriz, &ordem);
+    printf("[ <- ] Deseja testar outra matriz? (S/n) > ");
+    scanf(" %c", &esc);
+  }while(esc == 'S' || esc == 's');
+  return 0;
+}
