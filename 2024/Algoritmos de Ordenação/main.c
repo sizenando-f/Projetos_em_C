28a662581bb18591f76379e185b3881a32542311
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-09-12 10:11:12 -0400

    Algoritmos de bubblesort original e melhorado

--- 2024/Algoritmos de Ordenação/main.c
+++ 2024/Algoritmos de Ordenação/main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
-int main(){
+// Opção 1
+void bubbleSortOriginal(int vetor[], unsigned n){
+  for(unsigned i = 0; i < n; i++){
+    for(unsigned j = 0; j < n-1; j++){
+      if(vetor[j] > vetor[j+1]){
+        int aux = vetor[j+1];
+        vetor[j+1] = vetor[j];
+        vetor[j] = aux;
+      }
+    }
+  }
+}
 
+// Opção 2
+void bubbleSortMelhorado(int vetor[], unsigned n){
+  int mudou = 1;
+  unsigned nLinha = n, guarda = n;
+
+  while(mudou){
+    unsigned j = 0;
+    mudou = 0;
+    while(j < nLinha){
+      if(vetor[j] > vetor[j+1]){
+        int aux = vetor[j+1];
+        vetor[j+1] = vetor[j];
+        vetor[j] = aux;
+        mudou = 1;
+        guarda = j;
+      }
+      j++;
+    }
+    nLinha = guarda;
+  }
+}
+
+int main(){
+  int vetor[] = {1,5,6,2,4,8,3,4,98,0};
+
+  // bubbleSortOriginal(vetor, 10);
+  bubbleSortMelhorado(vetor, 10);
+
+  for(int i = 0; i < 10; i++){
+    printf("%d\n", vetor[i]);
+  }
   return 0;
 }
