61e326626983e0b568bb5734b4bd85f4c2d9f380
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-10-21 07:42:50 -0400

    Lógica de cálculo implementada

--- 2024/Portas Lógicas/Portas Lógicas/Portas Lógicas.cpp
+++ 2024/Portas Lógicas/Portas Lógicas/Portas Lógicas.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
 #include "Componentes.h"
+#include "Resolve.h"
 
 using namespace std;
 
 int main()
 {
+	string texto = "s6";
+	bool teste = texto[0] == 's';
+	cout << teste;
 }
--- 2024/Portas Lógicas/Portas Lógicas/Resolve.cpp
+++ 2024/Portas Lógicas/Portas Lógicas/Resolve.cpp
@@ -4,25 +4,39 @@ using namespace std;
 
 void Resolve::insere(Base* componente)
 {
-	componentes.push_back(componente->alocar());
+	componentes.push_back(componente->alocar());	// Aloca o componente no vetor 
 }
 
 void Resolve::resolveTotal()
 {
 	for (const auto& componente : componentes) {
-		vector<string> entradasIndices = componente->retornaEntradas();
-		vector<string> saidasIndices = componente->retornaSaidas();
+		vector<string> entradasIndices = componente->retornaEntradas(); // Recebe as entradas a quais os componentes estão conectados
+		vector<string> saidasIndices = componente->retornaSaidas();	// Recebe as saídas a quais os componentes estão conectados
 		vector<int> entradas;
 
 		for (const auto& indices : entradasIndices) {
-			entradas.push_back(campo[indices[0] - '0'][7 - (indices[1] - '0')]);
+			if (indices != "") {	// Se houver índices de entrada
+				if (indices[0] == 'e') {	// Se estiver conectado no vetor de entradas
+					entradas.push_back(entrada[indices[1] - '0']); // Pega o que está no vetor de entradas a partir do índice convertido para inteiro
+				}
+				else {
+					entradas.push_back(campo[indices[0] - '0'][7 - (indices[1] - '0')]); // Pega o que está na matriz a partir dos índices convertido para inteiro
+				}
+			}
 		}
 
-		vector<bool> resultados = componente->calcula(entradas);
+		vector<bool> resultados = componente->calcula(entradas);	// Armazena os resultados do componente num vetor pra caso tenha mais de uma saída
 
+		int i = 0;
 		for (const auto& indices : saidasIndices) {
-			for (const auto& resultado : resultados) {
-				campo[indices[0] - '0'][7 - (indices[1] - '0')] = resultado;
+			if (indices != "") {	// Se houver índices de saída
+				if (indices[0] == 's') {	// Se estiver conectado no vetor de saída
+					saida[indices[1] - '0'] = resultados[i];	// Insere o resultado no vetor de saída
+				}
+				else {
+					campo[indices[0] - '0'][7 - (indices[1] - '0')] = resultados[i];	// Insere o resultado no índice convertido para inteiro
+				}
+				i++;	// Incrementa para ir para próximo resultado caso exista
 			}
 		}
 		
@@ -32,6 +46,6 @@ void Resolve::resolveTotal()
 Resolve::~Resolve()
 {
 	for (const auto& componente : componentes) {
-		delete componente;
+		delete componente;	// Desaloca cada componente 
 	}
 }
