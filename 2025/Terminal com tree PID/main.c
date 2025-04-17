09a337c56076c290a2fb87e627ac7719822b07de
Sizenando França <sizenandofranca290@gmail.com>
2025-04-16 20:59:12 -0400

    Modularização do código e adição de comentários Doxygen

--- 2025/Terminal com tree PID/main.c
+++ 2025/Terminal com tree PID/main.c
@@ -1,3 +1,14 @@
+/**
+    * @file main.c
+    * @brief Programa para exibir processos em formato de árvore e executar comandos no terminal.
+    *
+    * Este programa lê os dados de processos do diretório /proc e exibe os processos em formato de árvore.
+    * Também permite a execução de comandos no terminal, incluindo a execução em segundo plano.
+    *
+    * @author Sizenando S. França
+    * @date 16-04-2025
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,13 +17,24 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-struct Process {    // Estrutura para guardar os dados do processo
-    int pid;
-    int ppid;
-    char estado;
-    char nome[256];
+struct Process {    // Estrutura para armazenar os dados do processo
+    int pid;    // PID do processo
+    int ppid;   // PID do processo pai
+    char estado;    // Estado do processo
+    char nome[256]; // Nome do processo
 };
 
+/**
+    * @brief Lê os dados do processo a partir do arquivo /proc/[pid]/stat.
+    *
+    * Esta função lê os dados do processo a partir do arquivo /proc/[pid]/stat
+    * e armazena as informações em uma estrutura de processos.
+    *
+    * @param pid PID do processo a ser lido.
+    * @param processos Array de estruturas Process para armazenar os dados dos processos.
+    * @param nProcessos Ponteiro para o número total de processos lidos.
+    * @return void
+*/
 void le_processos(int pid, struct Process *processos, int *nProcessos){ // Lê os dados do processo a partir do /proc/[pid]/stat
     char path[256]; // Buffer para guardar o caminho do arquivo
     snprintf(path, sizeof(path), "/proc/%d/stat", pid); // Cria o caminho do arquivo
@@ -31,7 +53,18 @@ void le_processos(int pid, struct Process *processos, int *nProcessos){ // Lê o
     fclose(fp); // Fecha o arquivo
 }
 
-void inicializa_processos(struct Process *processos, int *nProcessos){ // Inicializa a estrutura de processos
+/**
+    * @brief Inicializa a estrutura de processos.
+    *
+    * Esta função lê os dados de todos os processos do diretório /proc e armazena
+    * as informações em uma estrutura de processos.
+    *
+    * @param processos Array de estruturas Process para armazenar os dados dos processos.
+    * @param nProcessos Ponteiro para o número total de processos lidos.
+    * @return void  
+    * @note Se algum processo não aparecer na árvore, então o processo não existe na pasta /proc
+*/
+void inicializa_processos(struct Process *processos, int *nProcessos){
     DIR *dir = opendir("/proc"); // Abre o diretório /proc
 
     if(dir == NULL){ // Verifica se o diretório foi aberto corretamente
@@ -51,106 +84,171 @@ void inicializa_processos(struct Process *processos, int *nProcessos){ // Inicia
     closedir(dir); // Fecha o diretório
 }
 
+/**
+    * @brief Exibe os processos em formato de árvore.
+    *
+    * Esta função exibe os processos em formato de árvore, começando pelo PID fornecido.
+    * A função é chamada recursivamente para exibir os processos filhos.
+    *
+    * @param ppid PID do processo pai.
+    * @param nivel Nível atual na árvore de processos.
+    * @param processos Array de estruturas Process contendo os dados dos processos.
+    * @param nProcessos Número total de processos lidos.
+    * @return void
+*/
 void exibir_processos(int ppid, int nivel, struct Process *processos, int nProcessos){
-    for(int i = 0; i < nProcessos; i++){
-        if(ppid == processos[i].pid){
-            printf("%*s", nivel * 4, ""); // Indenta a saída
-            printf("--- PID: %d nome: %s estado:%c\n", processos[i].pid, processos[i].nome, processos[i].estado); // Exibe os dados do processo
+    int nivelAtual = nivel * 4; // Nível atual na árvore de processos
+    for(int i = 0; i < nProcessos; i++){   // Percorre todos os processos lidos
+        if(ppid == processos[i].pid){   // Se o PID do processo for igual ao PID fornecido
+            printf("%*s", nivelAtual, ""); // Indenta a saída
+            printf("----- %s %d %c %d\n", processos[i].nome, processos[i].pid, processos[i].estado, processos[i].ppid); // Exibe os dados do processo
             break;
         }
     }
     
-    for(int i = 0; i < nProcessos; i++){
-        if(ppid == processos[i].ppid){
+    for(int i = 0; i < nProcessos; i++){    // Percorre todos os processos lidos
+        if(ppid == processos[i].ppid){  // Se o PID do processo pai for igual ao PID fornecido
+            printf("%*s", nivelAtual + 4, ""); // Indenta a saída
+            printf("|\n");  // Exibe a linha de conexão entre o processo pai e o filho
             exibir_processos(processos[i].pid, nivel + 1, processos, nProcessos); // Chama a função recursivamente para exibir os processos filhos
         }
-    }
-    
+    }   
 }
 
+
+/**
+ * @brief Executa o comando "tree" para exibir os processos em formato de árvore.
+ *
+ * Esta função inicializa a estrutura de processos, verifica se um PID válido foi fornecido
+ * e exibe os processos em formato de árvore a partir do PID fornecido.
+ *
+ * @param token Ponteiro para o token que contém o comando e os argumentos.
+ * @return void
+ */
+void execucao_tree(char *token){
+    int nProcessos = 0; // Número de processos lidos
+    struct Process processos[5000]; // Array de processos (Aumentar se necessário)
+    inicializa_processos(processos, &nProcessos); // Inicializa a estrutura de processos
+    
+    token = strtok(NULL, " "); // Lê o próximo token
+    if(token == NULL){ // Se não houver nenhum token, continua o loop
+        printf("[ #<- ] ERRO: Nenhum PID fornecido.\n");
+        return;
+    }
+
+    int pid = atoi(token); // Converte o token para inteiro
+
+    if(pid <= 0){ // Verifica se o PID é válido
+        printf("[ #<- ] ERRO: PID inválido.\n");
+        return;
+    }
+
+    exibir_processos(pid, 1, processos, nProcessos);    // Exibe os processos em árvore
+}
+
+/**
+    * @brief Executa um comando no terminal.
+    *
+    * Esta função executa um comando no terminal, criando um processo filho caso desejado
+    * para executar o comando e aguardando sua conclusão.
+    *
+    * @param arguments Array de argumentos do comando a ser executado.
+    * @param token Token que contém o comando a ser executado.
+    * @return void
+*/
+void terminal(char *argumentos[7], char *token){
+    int i = 0;  // Inicializa o índice para os argumentos
+    int ehFork = 0; // Variável para verificar se o comando deve ser executado em segundo plano
+
+    argumentos[i] = token;   // Armazena o primeiro token (comando) no vetor de argumentos
+    i++;    // Incrementa o índice
+    token = strtok(NULL, " ");  // Divide a linha em tokens usando espaço como delimitador
+
+    while(token != NULL){   // Continua a dividir a linha em tokens
+        if(i == 6){ // Verifica se o número máximo de argumentos foi atingido
+            printf("[ #<- ] ERRO: Número máximo de argumentos excedido.\n");
+            break;
+        }
+
+        if (token[0] == '&') {  // Verifica se o fork será criado
+            ehFork = 1;
+            break;
+        }
+        
+        argumentos[i] = token; // Armazena o token no vetor de argumentos
+        i++;
+        token = strtok(NULL, " "); // Continua a dividir a linha em tokens
+    }
+    argumentos[i] = NULL; // Adiciona NULL ao final do vetor de argumentos, necessário para o execvp
+    
+
+    pid_t pid = fork(); // Cria um novo processo
+    if(pid == -1){  // Verifica se o fork falhou
+        perror("[ -># ] ERRO: Não foi possível criar o processo filho");
+        return;
+    }
+    
+    if(pid == 0){ // O que o processo filho deve fazer
+        execvp(argumentos[0], argumentos); // Executa o comando com os argumentos fornecidos, processo filho morre mas o pai continua rodando
+        perror("[ -># ] ERRO: Não foi possível executar o comando");
+        exit(EXIT_FAILURE); // Se o execvp falhar, o processo filho termina com erro 
+    } else {
+        if(!ehFork){  // O que o processo pai deve fazer se não for um fork
+            waitpid(pid, NULL, 0);  // Espera o processo filho terminar
+        }
+    }
+
+}
+
+/**
+    * @brief Função principal do programa.
+    *
+    * Esta função lê os comandos do usuário, executa os comandos apropriados
+    * e exibe os processos em formato de árvore quando solicitado.
+    *
+    * @return 0 se o programa for executado com sucesso.
+*/
 int main(){
     char input[200]; // Buffer para guardar a linha de comando
     char *token; // Ponteiro para guardar os tokens 
-    char *arguments[7]; // Ponteiro para guardar os argumentos
+    char *argumentos[7]; // Ponteiro para guardar os argumentos
+
+    system("clear");
+    printf("-------------------------------------------------------------------------------------\n");
+    printf("[ ! ] Informações de Uso:\n");
+    printf("  - Digite 'exit' para sair do programa.\n");
+    printf("  - Digite 'tree PID' para exibir os processos em formato de árvore do PID desejado.\n");
+    printf("  - Para executar um comando em segundo plano, adicione '&' como argumento final.\n");
+    printf("  - O terminal suporta 5 argumentos, qualquer coisa além disso será ignorado.\n\n");
+
+    printf("[ ! ] Informações de Execução:\n");
+    printf("  - [ #<- ] : Erro de entrada do usuário.\n");
+    printf("  - [ -># ] : Erro interno de execução.\n");
+    printf("  - [ <- ]  : Entrada do usuário.\n");
+    printf("  - [ -> ]  : Saída para o usuário.\n");
+    printf("-------------------------------------------------------------------------------------\n");
+
 
     while(1){
-        printf("> ");
+        printf("[ <- ] ");
         fgets(input, sizeof(input), stdin); // Lê a linha de comando do usuário
         input[strcspn(input, "\n")] = '\0'; // Remove o caractere de nova linha
 
         token = strtok(input, " "); // Divide a linha em tokens usando espaço como delimitador
         if(token == NULL){  // Se não houver nenhum token, continua o loop
-            printf("Erro: Nenhum comando fornecido.\n");
+            printf("[ #<- ] ERRO: Nenhum comando fornecido.\n");
             continue;
         }
 
-        if(strcmp(token, "tree") == 0){ // Comando para listar os processos em árvore
-            int nProcessos = 0; // Número de processos lidos
-            struct Process processos[5000]; // Array de processos
-            inicializa_processos(processos, &nProcessos); // Inicializa a estrutura de processos
-            
-            token = strtok(NULL, " "); // Lê o próximo token
-            if(token == NULL){ // Se não houver nenhum token, continua o loop
-                printf("Erro: Nenhum PID fornecido.\n");
-                continue;
-            }
-
-            int pid = atoi(token); // Converte o token para inteiro
-
-            if(pid <= 0){ // Verifica se o PID é válido
-                printf("Erro: PID inválido.\n");
-                continue;
-            }
-
-            exibir_processos(pid, 1, processos, nProcessos);
-
-            
+        if(strcmp(token, "tree") == 0){
+            execucao_tree(token);   // Executa a função para exibir os processos em árvore
+            printf("-------------------------------------------------------------------------------------\n");
         } else if (strcmp(token, "exit") == 0){
+            printf("[ -> ] Saindo do programa...\n");
             break;
         } else {
-            int i = 0;
-            int isFork = 0;
-
-            arguments[i] = token;
-            i++;
-            token = strtok(NULL, " ");
-            while(token != NULL){
-                if(i == 6){
-                    printf("Erro: Número máximo de argumentos excedido.\n");
-                    break;
-                }
-
-                if (token[0] == '&') {
-                    isFork = 1;
-                    break;
-                }
-                
-                arguments[i] = token; // Armazena o token no vetor de argumentos
-                i++;
-                token = strtok(NULL, " "); // Continua a dividir a linha em tokens
-            }
-            arguments[i] = NULL; // Adiciona NULL ao final do vetor de argumentos
-            
-
-            pid_t pid = fork();
-            if(pid == -1){
-                perror("Erro ao criar o processo filho");
-                continue;
-            }
-            
-            if(pid == 0){ // O que o processo filho deve fazer
-                execvp(arguments[0], arguments); // Executa o comando com os argumentos fornecidos, processo filho morre mas o pai continua rodando
-                perror("Erro ao executar o comando");
-                exit(EXIT_FAILURE); // Se o execvp falhar, o processo filho termina com erro 
-            } else {
-                if(!isFork){  // O que o processo pai deve fazer
-                    waitpid(pid, NULL, 0);
-                }
-            }
-
+            terminal(argumentos, token); // Executa o comando fornecido
         }
-
-        
     }
     return 0;
 }
