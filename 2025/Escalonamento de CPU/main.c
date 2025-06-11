95e64b75cdc65d553c1bcc7a1cd348fc689d3698
Sizenando S. França <sizenandofranca290@gmail.com>
2025-06-11 15:35:11 -0400

    Término da leitura do arquivo

--- 2025/Escalonamento de CPU/main.c
+++ 2025/Escalonamento de CPU/main.c
@@ -10,15 +10,15 @@ typedef struct{
 /**
  * @brief Expande a capacidade de armazenamento de processos
  * 
- * O procedimento expande a capacidade do vetor de processos em 3
+ * O procedimento expande a capacidade do vetor de processos em 4
  * 
  * @param processos Vetor de processos a ser expandido
  * @param qtd_processos Quantidade de processos alocados 
  */
-void expandir_processos(Processo processos[], unsigned short int *qtd_processos){
-    // Aloca mais 3 espaços
+void expandir_processos(Processo **processos, unsigned short int *qtd_processos){
+    // Aloca mais 4 espaços
     *qtd_processos += 4;
-    Processo *temp = realloc(processos, *qtd_processos * sizeof(Processo));
+    Processo *temp = realloc(*processos, *qtd_processos * sizeof(Processo));
     // Se der errado encerra
     if(temp == NULL){
         puts("[ -># ] Erro ao expandir processos!");
@@ -26,7 +26,7 @@ void expandir_processos(Processo processos[], unsigned short int *qtd_processos)
     }
 
     // Atualiza para vetor original
-    processos = temp;
+    *processos = temp;
 }
 
 /**
@@ -40,7 +40,7 @@ void expandir_processos(Processo processos[], unsigned short int *qtd_processos)
  * @param qtd_processos Quantidade alocada para armazenar os processos 
  * @param processos_existentes Quantidade de processos existentes no vetor
  */
-void ler_arquivo(char nome_arquivo[], Processo processos[], unsigned short int *qtd_processos, unsigned short *processos_existentes){
+void ler_arquivo(char nome_arquivo[], Processo **processos, unsigned short int *qtd_processos, unsigned short *processos_existentes){
     // Abre arquivo
     FILE *fp = fopen(nome_arquivo, "r");
     if(fp == NULL){
@@ -59,13 +59,17 @@ void ler_arquivo(char nome_arquivo[], Processo processos[], unsigned short int *
         if(*qtd_processos == *processos_existentes){
             expandir_processos(processos, qtd_processos);
         }
-        // Aloca a quantidade inicial de 3 tempos para esse processo
-        processos[*processos_existentes].tam_tempos = 3;
-        processos[*processos_existentes].tempos = (unsigned*) malloc(processos[*processos_existentes].tam_tempos * sizeof(unsigned));
+        // Define o tamanho inicial do vetor de tempos
+        (*processos)[*processos_existentes].tam_tempos = 3;
+        // Aloca o vetor de tempos
+        (*processos)[*processos_existentes].tempos = (unsigned*) malloc((*processos)[*processos_existentes].tam_tempos * sizeof(unsigned));
 
-        // Parte para saber qual divisão da linha está lendo
-        // Temp para armazenar o valor lido da linha
-        unsigned short int parte = 1;
+        // Inicializa o processo com nenhum tempo
+        (*processos)[*processos_existentes].qtd_tempos = 0;
+
+        // Para saber qual divisão da linha está lendo
+        unsigned short int parte = 0;
+        // Para armazenar o valor lido da linha
         int temp;
 
         // Copia para buffer temporário e não afetar a linha original
@@ -74,39 +78,54 @@ void ler_arquivo(char nome_arquivo[], Processo processos[], unsigned short int *
         
         // Separação de cada valor da linha
         while(token != NULL){
+            // Conversão do token para inteiro
             temp = atoi(token);
-
+           
             switch (parte){
             // Prioridade
-            case 1:
+            case 0:
+                // Caso a prioridade estiver fora do intervalo permitido
                 if(temp < 1 || temp > 10){
                     printf("[ #<- ] A prioridade inserida na linha %d precisa estar entre 1 - 10\n", cont_linha);
                     exit(EXIT_FAILURE);
                 }
-                processos[*processos_existentes].prioridade = temp;
+                // Armazena a prioridade no processo
+                (*processos)[*processos_existentes].prioridade = temp;
                 break;
             // Instante de submissão
-            case 2:
-                processos[*processos_existentes].instante_submissao = temp;
+            case 1:
+                // Armazena o instante de submissão no processo
+                (*processos)[*processos_existentes].instante_submissao = temp;
                 break;
             default:
-                if(processos[*processos_existentes].tam_tempos < processos[*processos_existentes].qtd_tempos){
-                    processos[*processos_existentes].tam_tempos += 3;
-                    processos[*processos_existentes].tempos = realloc(processos[*processos_existentes].tempos, processos[*processos_existentes].tam_tempos * sizeof(int));
-                    if(processos[*processos_existentes].tempos == NULL){
+                // Verifica se o vetor de tempos está cheio
+                if((*processos)[*processos_existentes].tam_tempos <= (*processos)[*processos_existentes].qtd_tempos){
+                    // Expande o vetor em um valor ímpar
+                    (*processos)[*processos_existentes].tam_tempos += 3;
+                    // Realoca o vetor de tempos
+                    (*processos)[*processos_existentes].tempos = realloc((*processos)[*processos_existentes].tempos, (*processos)[*processos_existentes].tam_tempos * sizeof(int));
+                    // Se der errado, encerra programa
+                    if((*processos)[*processos_existentes].tempos == NULL){
                         puts("[ -># ] Erro ao expandir tempos do processo!");
                         exit(EXIT_FAILURE);
                     }
                 }
+                
+                // Armazena o tempo no vetor de tempos do processo, de 0 até a quantidade total de tempos
+                (*processos)[*processos_existentes].tempos[parte-2] = (unsigned) temp;
 
-                processos[*processos_existentes].qtd_tempos++;
+                // Incrementa o contador de quantidade de tempos que o processo possui
+                (*processos)[*processos_existentes].qtd_tempos++;
                 break;
             }
+            // Passa para a próxima parte da linha
             parte++;
+            // Avança o token
             token = strtok(NULL, " ");
         }
 
-        cont_linha++, processos_existentes++;
+        // Incrementa o contador de linha e a quantidade de processos existentes
+        cont_linha++, (*processos_existentes)++;
     }
 
     fclose(fp);
@@ -127,11 +146,12 @@ int main(int argc, char *argv[]){
     Processo *processos = (Processo*) malloc(qtd_processos * sizeof(Processo));
 
     // Faz leitura do arquivo passado
-    ler_arquivo(argv[1], processos, &qtd_processos, &processos_existentes);
+    ler_arquivo(argv[1], &processos, &qtd_processos, &processos_existentes);
 
     // Libera todas as memórias
-    for(unsigned short i = 0; i <= processos_existentes; i++){
+    for(unsigned short i = 0; i < processos_existentes; i++){
         free(processos[i].tempos);
+
     }
     free(processos);
     return 0;
