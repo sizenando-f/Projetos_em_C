2b02872a9d85bb21beee610f480a8b609868306e
sizenando-f <sizenandofranca290@gmail.com>
2025-07-18 14:55:45 -0400

    Criação inicial de cliente e servidor

---
+++ 2025/Serviços de Cotação de Preços de Combustíveis/client.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    // Cria o socket (telefone) do cliente
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);;
+    if(client_socket == -1){
+        cerr << "Erro ao criar o socket do cliente!" << endl;
+        return 1;
+    }
+
+    // Define que tipo de endereço o cliente vai se conectar e a porta do servidor
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+
+    // Converte o endereço em string para o formato binario
+    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0){
+        cerr << "Endereço IP inválido ou não suportado" << endl;
+        return 1;
+    }
+
+    // Conecta o socket criado do cliente no endereço desejado
+    if(connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+        cerr << "Erro na conexão com o servidor" << endl;
+        return 1;
+    } 
+
+    cout << "Conectado ao servidor!" << endl;
+
+    // Se comunica com o servidor seja enviando uma mensagem ou recebendo
+    const char *message = "Olá, servidor!";
+    send(client_socket, message, strlen(message), 0);
+    cout << "Mensagem enviada ao servidor!" << endl;
+
+    char buffer[1024] = {0};
+    read(client_socket, buffer, 1024);
+    cout << "Resposta do servidor:" << buffer << endl;
+
+    close(client_socket);
+
+    return 0;
+}
---
+++ 2025/Serviços de Cotação de Preços de Combustíveis/multiserver.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <thread>
+
+using namespace std;
+
+void handle_client(int client_socket){
+    cout << "Novo thread criado para cuidar do cliente socket: " << client_socket << endl;
+
+    char buffer[1024] = {0};
+    read(client_socket, buffer, 1024);
+    cout << "Mensagem do cliente [" << client_socket << "]: " << buffer << endl;
+
+    const char *message = "Olá, eu sou o garçom-thread! Bem-vindo!";
+    send(client_socket, message, strlen(message), 0);
+
+    close(client_socket);
+    cout << "Conexão com o cliente " << client_socket << " fechada" << endl;
+}
+
+int main(){
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd == -1){
+        cerr << "Erro ao criar o socket do servidor" << endl;
+        return 1;
+    }
+
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(8080);
+
+    if(bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+        cerr << "Erro ao fazer o bind" << endl;
+        return 1;
+    }
+
+    if(listen(server_fd, 5) < 0){
+        cerr << "Erro no listen" << endl;
+        return 1;
+    }
+
+    cout << "Servidor multi-cliente ouvindo na porta 8080..." << endl;
+
+    while(true){
+        sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        int client_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_len);
+
+        if(client_socket < 0){
+            cerr << "Erro ao aceitar conexão" << endl;
+            continue;
+        }
+
+        cout << "Cliente conectado! Criando uma thread para ele..." << endl;
+
+        thread client_thread(handle_client, client_socket);
+        client_thread.detach();
+    }
+
+    close(server_fd);
+
+    return 0;
+}
