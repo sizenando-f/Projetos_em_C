25013aa89484fab0fb97e5f60f75120ae3734003
sizenando-f <sizenandofranca290@gmail.com>
2025-08-16 09:21:46 -0400

    fix(threads): Corrige número de threads e robustez geral

--- 2025/Semáforos de Contagem/main.c
+++ 2025/Semáforos de Contagem/main.c
@@ -1,15 +1,15 @@
 /**
  * @file main.c
- * 
+ *
  * Esse arquivo contém a implementação do trabalho de S.O. sobre
  * o uso de semáforos para contagem. De forma concorrente é realizo:
  * 1. Leitura de um arquivo
  * 2. Filtro por valores ímpares e menor/igual a 2
  * 3. Filtro de números primos
  * 4. Impressão do resultado
- * 
+ *
  * @author Sizenando S. França | RGM: 50575
- * @date 13-08-2025
+ * @date 16-08-2025
  */
 
 #include <stdio.h>     // Entrada, saída, leitura de arquivo
@@ -134,75 +134,34 @@ void inicializa_lista(Lista *lista)
 }
 
 /**
- * @brief Cria lista 1 com valores do arquivo
+ * @brief Cria lista 2 com impares
  *
- * Thread que lê os valores de um arquivo desejado e insere numa
- * lista simplesmente encadeada
- *
- */
-void *le_arquivo(void *arg)
-{
-    // Abre arquivo e verifica se foi aberto corretamente
-    FILE *arquivo = fopen(nome_arquivo, "r");
-    if (arquivo == NULL)
-    {
-        perror("Erro ao abrir o arquivo!");
-        pthread_exit(NULL);
-    }
-
-    // Lê valor por valor
-    int valor;
-    while (fscanf(arquivo, "%d", &valor) != EOF)
-    {
-        // Bloqueia o acesso à lista 1
-        sem_wait(mutex1);
-        // Adiciona o valor na lista
-        adiciona_no(&L1, valor);
-        // Libera o acesso à lista 1
-        sem_post(mutex1);
-        // Sinaliza que lista 1 possui valor
-        sem_post(cont1);
-    }
-    // Fecha arquivo
-    fclose(arquivo);
-
-    // Bloqueia acesso à lista 1
-    sem_wait(mutex1);
-    // Adiciona flag -1 para indicar final da lista
-    adiciona_no(&L1, -1);
-    // Libera acesso à lista 1
-    sem_post(mutex1);
-    // Sinaliza que lista 1 possui mais um valor
-    sem_post(cont1);
-
-    // Encerra a thread
-    pthread_exit(NULL);
-}
-
-/**
- * @brief Cria lista 2 com impares 
- * 
  * Thread que cria uma lista encadeada apenas com os números ímpares ou
  * valores <= 2
- * 
+ *
  */
-void* remove_pares(void * arg){
+void *remove_pares(void *arg)
+{
     // Variável para receber  o valor da lista
     int valor;
-    while(1){
+    while (1)
+    {
         // Espera pela lista 1 ter algum elemento novo
         sem_wait(cont1);
         // Assume o controle da lista
         sem_wait(mutex1);
-        // Lê e remove o valor do nó cabeça 
+        // Lê e remove o valor do nó cabeça
         valor = remove_no(&L1);
         // Devolve o acesso à lista
         sem_post(mutex1);
 
         // Se for impar oi menor, valor é inserido na lista 2
-        if(valor == -1){
+        if (valor == -1)
+        {
             break;
-        } else if((valor % 2 == 1) || valor <= 2){
+        }
+        else if ((valor % 2 == 1) || valor <= 2)
+        {
             // Toma acesso da lista 2
             sem_wait(mutex2);
             // Adiciona valor na nova lista
@@ -222,21 +181,31 @@ void* remove_pares(void * arg){
     sem_post(mutex2);
     // Sinaliza que lista contém novo nó
     sem_post(cont2);
+
+    return NULL;
 }
 
 /**
  * @brief Verifica se valor é primo
- * 
+ *
  * Função que verifica se valor passado é primo
- * 
+ *
  * @param valor Valor para ser verificado
- * 
+ *
  * @return 1 se for primo e 0 se não for primo
  */
-int verifica_primo(int valor){
-    if(valor > 1){
-        for(int i = 2; i < valor; i++){
-            if(valor % i == 0){
+int verifica_primo(int valor)
+{
+    if (valor <= 1)
+    {
+        return 0;
+    }
+    else
+    {
+        for (int i = 2; i < valor; i++)
+        {
+            if (valor % i == 0)
+            {
                 return 0;
             }
         }
@@ -246,13 +215,15 @@ int verifica_primo(int valor){
 
 /**
  * @brief Cria lista 3 com primos
- * 
+ *
  * Cria uma lista simplesmente encadeada contendo apenas valores primos
  * da lista 2
  */
-void* filtra_primos(void *arg){
-    int valor, is_primo;
-    while(1){
+void *filtra_primos(void *arg)
+{
+    int valor;
+    while (1)
+    {
         // Verifica se lista 2 possui novo nó
         sem_wait(cont2);
         // Recebe acesso a lista 2
@@ -263,13 +234,15 @@ void* filtra_primos(void *arg){
         sem_post(mutex2);
 
         // Se não for valor terminal
-        if(valor == -1){
+        if (valor == -1)
+        {
             break;
-        } else {
-            // Verifica se o valor lido é primo
-            is_primo = verifica_primo(valor);
+        }
+        else
+        {
             // Se for primo
-            if(is_primo){
+            if (verifica_primo(valor))
+            {
                 // Ganha o acesso a lista 3
                 sem_wait(mutex3);
                 // Adiciona o valor primo na lista
@@ -290,17 +263,21 @@ void* filtra_primos(void *arg){
     sem_post(mutex3);
     // Sinaliza que lista contém novo nó
     sem_post(cont3);
+
+    return NULL;
 }
 
 /**
  * @brief Imprime valores de L3
- * 
+ *
  * Imprime todos valores existentes na lista 3
  */
-void* imprime_primos(void *arg){
+void *imprime_primos(void *arg)
+{
     int valor;
-    printf("[ -> ] Valores primos encontrados: \n|");
-    while(1){
+    printf("[ -> ] Valores primos encontrados: \n");
+    while (1)
+    {
         // Verifica se lista 2 possui novo nó
         sem_wait(cont3);
         // Recebe acesso a lista 2
@@ -311,21 +288,27 @@ void* imprime_primos(void *arg){
         sem_post(mutex3);
 
         // Se não for valor terminal, imprime na tela
-        if(valor == -1){
+        if (valor == -1)
+        {
             break;
-        } else {
-            printf("%d| ", valor);
+        }
+        else
+        {
+            printf("%d ", valor);
         }
     }
+
+    return NULL;
 }
 
 /**
  * @brief Iniciliza semáforos
- * 
+ *
  * Realiza o unlink de possíveis semáforos abertos e abre novos com
  * seus respectivos valores iniciais
  */
-void inicializa_semaforos(){
+void inicializa_semaforos()
+{
     // Realiza o unlink de possíveis semáforos abertos anteriormente
     sem_unlink(CONT1_NOME);
     sem_unlink(CONT2_NOME);
@@ -336,68 +319,118 @@ void inicializa_semaforos(){
 
     // Realiza a abertura de cada semáforo
     cont1 = sem_open(CONT1_NOME, O_CREAT, 0660, 0);
-    if(cont1 == SEM_FAILED){
+    if (cont1 == SEM_FAILED)
+    {
         perror("Erro ao criar cont1");
         exit(EXIT_FAILURE);
     }
 
     cont2 = sem_open(CONT2_NOME, O_CREAT, 0660, 0);
-    if(cont2 == SEM_FAILED){
+    if (cont2 == SEM_FAILED)
+    {
         perror("Erro ao criar cont2");
         exit(EXIT_FAILURE);
     }
 
     cont3 = sem_open(CONT3_NOME, O_CREAT, 0660, 0);
-    if(cont3 == SEM_FAILED){
+    if (cont3 == SEM_FAILED)
+    {
         perror("Erro ao criar cont3");
         exit(EXIT_FAILURE);
     }
 
     mutex1 = sem_open(MUTEX1_NOME, O_CREAT, 0660, 1);
-    if(mutex1 == SEM_FAILED){
+    if (mutex1 == SEM_FAILED)
+    {
         perror("Erro ao criar mutex1");
         exit(EXIT_FAILURE);
     }
 
     mutex2 = sem_open(MUTEX2_NOME, O_CREAT, 0660, 1);
-    if(mutex2 == SEM_FAILED){
+    if (mutex2 == SEM_FAILED)
+    {
         perror("Erro ao criar mutex2");
         exit(EXIT_FAILURE);
     }
 
     mutex3 = sem_open(MUTEX3_NOME, O_CREAT, 0660, 1);
-    if(mutex3 == SEM_FAILED){
+    if (mutex3 == SEM_FAILED)
+    {
         perror("Erro ao criar mutex3");
         exit(EXIT_FAILURE);
     }
 }
 
+// "Thread principal"
 int main(int argc, char **argv)
 {
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("[ #<- ] Uso: %s <nome_arquivo>", argv[0]);
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
+        // Define as threads
+        pthread_t thread_impares, thread_primos, thread_imprime;
+
+        // Salva o nome do arquivo
         strcpy(nome_arquivo, argv[1]);
 
-        pthread_t thread_le, thread_impares, thread_primos, thread_imprime;
-
+        // Incializa cada lista com NULL
         inicializa_lista(&L1);
         inicializa_lista(&L2);
         inicializa_lista(&L3);
 
+        // Inicializa todos mutex e contadores
         inicializa_semaforos();
 
-        pthread_create(&thread_le, NULL, le_arquivo, NULL);
+        // Cria todas as threads
+        // pthread_create(&thread_le, NULL, principal, NULL);
         pthread_create(&thread_impares, NULL, remove_pares, NULL);
         pthread_create(&thread_primos, NULL, filtra_primos, NULL);
         pthread_create(&thread_imprime, NULL, imprime_primos, NULL);
 
-        pthread_join(thread_le, NULL);
+        // Abre arquivo e verifica se foi aberto corretamente
+        FILE *arquivo = fopen(nome_arquivo, "r");
+        if (arquivo == NULL)
+        {
+            perror("Erro ao abrir o arquivo!");
+            exit(EXIT_FAILURE);
+        }
+
+        // Lê valor por valor
+        int valor;
+        while (fscanf(arquivo, "%d", &valor) != EOF)
+        {
+            // Bloqueia o acesso à lista 1
+            sem_wait(mutex1);
+            // Adiciona o valor na lista
+            adiciona_no(&L1, valor);
+            // Libera o acesso à lista 1
+            sem_post(mutex1);
+            // Sinaliza que lista 1 possui valor
+            sem_post(cont1);
+        }
+        // Fecha arquivo
+        fclose(arquivo);
+
+        // Bloqueia acesso à lista 1
+        sem_wait(mutex1);
+        // Adiciona flag -1 para indicar final da lista
+        adiciona_no(&L1, -1);
+        // Libera acesso à lista 1
+        sem_post(mutex1);
+        // Sinaliza que lista 1 possui mais um valor
+        sem_post(cont1);
+
+        // Define cada uma para esperar
+        // pthread_join(thread_le, NULL);
         pthread_join(thread_impares, NULL);
         pthread_join(thread_primos, NULL);
         pthread_join(thread_imprime, NULL);
 
+        // Encerra todos os semáforos
         sem_close(cont1);
         sem_close(cont2);
         sem_close(cont3);
@@ -405,6 +438,7 @@ int main(int argc, char **argv)
         sem_close(mutex2);
         sem_close(mutex3);
 
+        // Apaga o link com os semáforos salvos
         sem_unlink(CONT1_NOME);
         sem_unlink(CONT2_NOME);
         sem_unlink(CONT3_NOME);
